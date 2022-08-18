f8afae13690508ff40ee2f6e41c3c38ca3b18933
LJL <kpnprod@gmail.com>
2022-08-18 11:42:35 +0200

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'oc14-modal-library'
+import 'oc14-modal-library/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
